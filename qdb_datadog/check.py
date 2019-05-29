adc498dc107c4f6fec8c838e5ddb135053d0c759
Leon Mergen <leon@solatis.com>
2019-05-29 12:35:06 +0200

    Support establishing secure connections

--- qdb_datadog/check.py
+++ qdb_datadog/check.py
@@ -4,6 +4,7 @@ import argparse
 import quasardb
 import requests
 
+import json
 from functools import reduce
 from .metrics import key_to_metric
 from .metrics import MetricType
@@ -19,9 +20,20 @@ def get_args():
         default="qdb://127.0.0.1:2836")
 
     parser.add_argument(
+        '--cluster-public-key',
+        dest='cluster_public_key',
+        help='Cluster public key file')
+
+    parser.add_argument(
+        '--user-security-file',
+        dest='user_security_file',
+        help='User security file, containing both username and private access token.')
+
+    parser.add_argument(
         '--node-id',
         dest='node_id',
         help='Node id to collect metrics from, e.g. 0-0-0-1')
+
 
     parser.add_argument(
         '--prefix',
@@ -32,8 +44,25 @@ def get_args():
 
     return parser.parse_args()
 
-def get_qdb_conn(uri):
-    return quasardb.Cluster(uri)
+def _parse_user_security_file(x):
+    with open(x, 'r') as fp:
+        parsed = json.load(fp)
+        return (parsed['username'], parsed['secret_key'])
+
+def _slurp(x):
+    with open(x, 'r') as fp:
+        return fp.read()
+
+def get_qdb_conn(uri, cluster_public_key=None, user_security_file=None):
+    if cluster_public_key and user_security_file:
+        user, private_key = _parse_user_security_file(user_security_file)
+        public_key = _slurp(cluster_public_key)
+        return quasardb.Cluster(uri,
+                                user_name=user,
+                                user_private_key=private_key,
+                                cluster_public_key=public_key)
+    else:
+        return quasardb.Cluster(uri)
 
 def parse_key(key):
     return key.split('.', 3)[-1]
@@ -95,7 +124,9 @@ def print_metrics(metrics, prefix=None, instance_id=None):
 def main():
     args = get_args()
 
-    conn = get_qdb_conn(args.cluster_uri)
+    conn = get_qdb_conn(args.cluster_uri,
+                        cluster_public_key=args.cluster_public_key,
+                        user_security_file=args.user_security_file)
     keys = collect_keys(conn, args.node_id)
     metrics = collect_metrics(conn, keys)
 
